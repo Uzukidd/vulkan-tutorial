6311b7e51ada40573466d50606f07b6aca6427ba
Uzuki Ishikawajima <uzukidd@gmail.com>
2025-03-01 15:22:10 +0800

    feat: Physical devices and queue families, Logical device and queues added.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 call "%VCVARS64%"
 
-SET includes=/Isrc /I%VULKAN_SDK%/Include /I%GLFW_SDK%/include
-SET links=/link /LIBPATH:%VULKAN_SDK%/Lib /LIBPATH:%GLFW_SDK%/lib-vc2022 vulkan-1.lib glfw3_mt.lib user32.lib gdi32.lib shell32.lib
+SET includes=/Isrc /I%VULKAN_SDK%/Include /I%GLFW_SDK%/include /I%GLM_SDK%
+SET links=/link /LIBPATH:%VULKAN_SDK%/Lib /LIBPATH:%GLFW_SDK%/lib-vc2019 vulkan-1.lib glfw3_mt.lib user32.lib gdi32.lib shell32.lib
 SET defines=/D DEBUG
 SET standard=/std:c++17
 SET optim=/O2
